780be9ac291c4058a145268682d1774eb02bd0a7
Evie <evita@stenqvist.co>
2022-04-27 14:18:22 +0200

    Tab styles align with JB styles

--- src/Tabs/TabList.svelte
+++ src/Tabs/TabList.svelte
@@ -3,7 +3,10 @@
 </div>
 
 <style>
+    /* TODO: the tabs aren't scrolling, they're stacking */
 	.tab-list {
-		border-bottom: 1px solid teal;
+		border-bottom: 1px solid var(--text-disabled);
+        overflow-x: scroll;
+        max-width: 100vw;
 	}
 </style>
