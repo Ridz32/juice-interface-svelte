7fed7f7d4112ff2a0a4febc091b507e85b72028e
EVMCompatible <evmcompatible@gmail.com>
2022-04-28 10:40:53 +0500

    update

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,28 +1,23 @@
 <script lang="ts">
-	import Footer from '$lib/sections/Footer.svelte';
-	import Header from '$lib/sections/Header.svelte';
+	import MobileHeader from '$lib/components/MobileHeader.svelte';
+	import Header from '$lib/components/Header.svelte';
 	import '../app.css';
 </script>
 
 <svelte:head>
-	<title>OneOf Foundation DAO</title>
+	<title>JuiceboxDAO</title>
 </svelte:head>
 
 <main>
 	<Header />
+	<MobileHeader />
 	<slot />
-	<Footer />
 </main>
 
 <style>
 	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 0;
-		width: 100%;
-		max-width: 1024px;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}	
+		overflow: hidden;
+	}
 </style>
