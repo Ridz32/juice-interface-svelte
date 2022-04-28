7d246086364169ffc084b3c128f03df35582c7bd
EVMCompatible <evmcompatible@gmail.com>
2022-04-28 10:41:00 +0500

    update

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,216 @@
+<header
+	class="ant-layout-header top-nav"
+	style="display: flex; justify-content: space-between; align-items: center; background: var(--background-l0);"
+>
+	<div
+		class="ant-space ant-space-horizontal ant-space-align-center top-left-nav"
+		style="flex: 1 1 0%; gap: 24px;"
+	>
+		<div class="ant-space-item" style="">
+			<a href="/" style="display: inline-block;">
+				<img src="/juice_logo-ol.png" alt="Juicebox logo" style="height: 40px;" />
+			</a>
+		</div>
+		<div class="ant-space-item" style="">
+			<a
+				class="nav-menu-item hover-opacity"
+				href="/#/projects"
+				style="cursor: pointer; display: flex; align-items: center; font-weight: 600; color: var(--text-primary);"
+				>Projects</a
+			>
+		</div>
+		<div class="ant-space-item" style="">
+			<a
+				href="/#/faq"
+				class="nav-menu-item hover-opacity"
+				style="cursor: pointer; display: flex; align-items: center; font-weight: 600; color: var(--text-primary);"
+				>FAQ</a
+			>
+		</div>
+		<div class="ant-space-item" style="">
+			<a
+				class="nav-menu-item hover-opacity"
+				href="https://discord.gg/6jXrJSyDFf"
+				target="_blank"
+				rel="noreferrer"
+				style="cursor: pointer; display: flex; align-items: center; font-weight: 600; color: var(--text-primary);"
+				>Discord</a
+			>
+		</div>
+		<div class="ant-space-item">
+			<div
+				class="ant-dropdown-trigger nav-menu-item hover-opacity ant-dropdown-open"
+				style="cursor: pointer; display: flex; align-items: center; font-weight: 600; color: var(--text-primary);"
+			>
+				Resources<span
+					role="img"
+					aria-label="down"
+					class="anticon anticon-down"
+					style="font-size: 13px; margin-left: 7px;"
+					><svg
+						viewBox="64 64 896 896"
+						focusable="false"
+						data-icon="down"
+						width="1em"
+						height="1em"
+						fill="currentColor"
+						aria-hidden="true"
+						><path
+							d="M884 256h-75c-5.1 0-9.9 2.5-12.9 6.6L512 654.2 227.9 262.6c-3-4.1-7.8-6.6-12.9-6.6h-75c-6.5 0-10.3 7.4-6.5 12.7l352.6 486.1c12.8 17.6 39 17.6 51.7 0l352.6-486.1c3.9-5.3.1-12.7-6.4-12.7z"
+						/></svg
+					></span
+				>
+			</div>
+		</div>
+	</div>
+	<div
+		class="ant-space ant-space-horizontal ant-space-align-center"
+		style="display: flex; align-items: center; gap: 16px;"
+	>
+		<div class="ant-space-item" style="">
+			<div class="language-selector" style="cursor: pointer;">
+				<span
+					role="img"
+					aria-label="global"
+					class="anticon anticon-global"
+					style="margin-bottom: 2px;"
+					><svg
+						viewBox="64 64 896 896"
+						focusable="false"
+						data-icon="global"
+						width="1em"
+						height="1em"
+						fill="currentColor"
+						aria-hidden="true"
+						><path
+							d="M854.4 800.9c.2-.3.5-.6.7-.9C920.6 722.1 960 621.7 960 512s-39.4-210.1-104.8-288c-.2-.3-.5-.5-.7-.8-1.1-1.3-2.1-2.5-3.2-3.7-.4-.5-.8-.9-1.2-1.4l-4.1-4.7-.1-.1c-1.5-1.7-3.1-3.4-4.6-5.1l-.1-.1c-3.2-3.4-6.4-6.8-9.7-10.1l-.1-.1-4.8-4.8-.3-.3c-1.5-1.5-3-2.9-4.5-4.3-.5-.5-1-1-1.6-1.5-1-1-2-1.9-3-2.8-.3-.3-.7-.6-1-1C736.4 109.2 629.5 64 512 64s-224.4 45.2-304.3 119.2c-.3.3-.7.6-1 1-1 .9-2 1.9-3 2.9-.5.5-1 1-1.6 1.5-1.5 1.4-3 2.9-4.5 4.3l-.3.3-4.8 4.8-.1.1c-3.3 3.3-6.5 6.7-9.7 10.1l-.1.1c-1.6 1.7-3.1 3.4-4.6 5.1l-.1.1c-1.4 1.5-2.8 3.1-4.1 4.7-.4.5-.8.9-1.2 1.4-1.1 1.2-2.1 2.5-3.2 3.7-.2.3-.5.5-.7.8C103.4 301.9 64 402.3 64 512s39.4 210.1 104.8 288c.2.3.5.6.7.9l3.1 3.7c.4.5.8.9 1.2 1.4l4.1 4.7c0 .1.1.1.1.2 1.5 1.7 3 3.4 4.6 5l.1.1c3.2 3.4 6.4 6.8 9.6 10.1l.1.1c1.6 1.6 3.1 3.2 4.7 4.7l.3.3c3.3 3.3 6.7 6.5 10.1 9.6 80.1 74 187 119.2 304.5 119.2s224.4-45.2 304.3-119.2a300 300 0 0010-9.6l.3-.3c1.6-1.6 3.2-3.1 4.7-4.7l.1-.1c3.3-3.3 6.5-6.7 9.6-10.1l.1-.1c1.5-1.7 3.1-3.3 4.6-5 0-.1.1-.1.1-.2 1.4-1.5 2.8-3.1 4.1-4.7.4-.5.8-.9 1.2-1.4a99 99 0 003.3-3.7zm4.1-142.6c-13.8 32.6-32 62.8-54.2 90.2a444.07 444.07 0 00-81.5-55.9c11.6-46.9 18.8-98.4 20.7-152.6H887c-3 40.9-12.6 80.6-28.5 118.3zM887 484H743.5c-1.9-54.2-9.1-105.7-20.7-152.6 29.3-15.6 56.6-34.4 81.5-55.9A373.86 373.86 0 01887 484zM658.3 165.5c39.7 16.8 75.8 40 107.6 69.2a394.72 394.72 0 01-59.4 41.8c-15.7-45-35.8-84.1-59.2-115.4 3.7 1.4 7.4 2.9 11 4.4zm-90.6 700.6c-9.2 7.2-18.4 12.7-27.7 16.4V697a389.1 389.1 0 01115.7 26.2c-8.3 24.6-17.9 47.3-29 67.8-17.4 32.4-37.8 58.3-59 75.1zm59-633.1c11 20.6 20.7 43.3 29 67.8A389.1 389.1 0 01540 327V141.6c9.2 3.7 18.5 9.1 27.7 16.4 21.2 16.7 41.6 42.6 59 75zM540 640.9V540h147.5c-1.6 44.2-7.1 87.1-16.3 127.8l-.3 1.2A445.02 445.02 0 00540 640.9zm0-156.9V383.1c45.8-2.8 89.8-12.5 130.9-28.1l.3 1.2c9.2 40.7 14.7 83.5 16.3 127.8H540zm-56 56v100.9c-45.8 2.8-89.8 12.5-130.9 28.1l-.3-1.2c-9.2-40.7-14.7-83.5-16.3-127.8H484zm-147.5-56c1.6-44.2 7.1-87.1 16.3-127.8l.3-1.2c41.1 15.6 85 25.3 130.9 28.1V484H336.5zM484 697v185.4c-9.2-3.7-18.5-9.1-27.7-16.4-21.2-16.7-41.7-42.7-59.1-75.1-11-20.6-20.7-43.3-29-67.8 37.2-14.6 75.9-23.3 115.8-26.1zm0-370a389.1 389.1 0 01-115.7-26.2c8.3-24.6 17.9-47.3 29-67.8 17.4-32.4 37.8-58.4 59.1-75.1 9.2-7.2 18.4-12.7 27.7-16.4V327zM365.7 165.5c3.7-1.5 7.3-3 11-4.4-23.4 31.3-43.5 70.4-59.2 115.4-21-12-40.9-26-59.4-41.8 31.8-29.2 67.9-52.4 107.6-69.2zM165.5 365.7c13.8-32.6 32-62.8 54.2-90.2 24.9 21.5 52.2 40.3 81.5 55.9-11.6 46.9-18.8 98.4-20.7 152.6H137c3-40.9 12.6-80.6 28.5-118.3zM137 540h143.5c1.9 54.2 9.1 105.7 20.7 152.6a444.07 444.07 0 00-81.5 55.9A373.86 373.86 0 01137 540zm228.7 318.5c-39.7-16.8-75.8-40-107.6-69.2 18.5-15.8 38.4-29.7 59.4-41.8 15.7 45 35.8 84.1 59.2 115.4-3.7-1.4-7.4-2.9-11-4.4zm292.6 0c-3.7 1.5-7.3 3-11 4.4 23.4-31.3 43.5-70.4 59.2-115.4 21 12 40.9 26 59.4 41.8a373.81 373.81 0 01-107.6 69.2z"
+						/></svg
+					></span
+				>
+				<div
+					class="ant-select medium ant-select-single ant-select-show-arrow"
+					style="display: flex; align-items: center; justify-content: space-evenly; cursor: pointer; height: 30px; font-weight: 500;"
+				>
+					<div class="ant-select-selector">
+						<span class="ant-select-selection-search"
+							><input
+								autocomplete="off"
+								type="search"
+								class="ant-select-selection-search-input"
+								role="combobox"
+								aria-expanded="false"
+								aria-haspopup="listbox"
+								aria-owns="rc_select_0_list"
+								aria-autocomplete="list"
+								aria-controls="rc_select_0_list"
+								aria-activedescendant="rc_select_0_list_0"
+								readonly={true}
+								unselectable={true}
+								value=""
+								style="opacity: 0;"
+								id="rc_select_0"
+							/></span
+						><span class="ant-select-selection-item" title="EN">EN</span>
+					</div>
+					<span
+						class="ant-select-arrow"
+						unselectable={true}
+						aria-hidden="true"
+						style="user-select: none;"
+						><span role="img" aria-label="down" class="anticon anticon-down ant-select-suffix"
+							><svg
+								viewBox="64 64 896 896"
+								focusable="false"
+								data-icon="down"
+								width="1em"
+								height="1em"
+								fill="currentColor"
+								aria-hidden="true"
+								><path
+									d="M884 256h-75c-5.1 0-9.9 2.5-12.9 6.6L512 654.2 227.9 262.6c-3-4.1-7.8-6.6-12.9-6.6h-75c-6.5 0-10.3 7.4-6.5 12.7l352.6 486.1c12.8 17.6 39 17.6 51.7 0l352.6-486.1c3.9-5.3.1-12.7-6.4-12.7z"
+								/></svg
+							></span
+						></span
+					>
+				</div>
+			</div>
+		</div>
+		<div class="ant-space-item" style="">
+			<div
+				class="clickable-border"
+				role="switch"
+				aria-checked="false"
+				style="display: flex; align-items: center; justify-content: space-evenly; cursor: pointer; width: 60px; margin-right: 10px; height: 30px; border-radius: 15px;"
+			>
+				<div
+					style="display: flex; align-items: center; justify-content: center; padding-top: 6px; padding-bottom: 6px; color: rgba(0, 0, 0, 0.933);"
+				>
+					<svg
+						viewBox="0 0 24 24"
+						width="18"
+						height="18"
+						fill="currentColor"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							d="M5.66 4.2L6.05 4.59C6.44 4.97 6.44 5.61 6.05 5.99L6.04 6C5.65 6.39 5.03 6.39 4.64 6L4.25 5.61C3.86 5.23 3.86 4.6 4.25 4.21L4.26 4.2C4.64 3.82 5.27 3.81 5.66 4.2Z"
+						/><path
+							d="M1.99 10.95H3.01C3.56 10.95 4 11.39 4 11.95V11.96C4 12.51 3.56 12.95 3 12.94H1.99C1.44 12.94 1 12.5 1 11.95V11.94C1 11.39 1.44 10.95 1.99 10.95Z"
+						/><path
+							d="M12 1H12.01C12.56 1 13 1.44 13 1.99V2.96C13 3.51 12.56 3.95 12 3.94H11.99C11.44 3.94 11 3.5 11 2.95V1.99C11 1.44 11.44 1 12 1Z"
+						/><path
+							d="M18.34 4.2C18.73 3.82 19.36 3.82 19.75 4.21C20.14 4.6 20.14 5.22 19.75 5.61L19.36 6C18.98 6.39 18.35 6.39 17.96 6L17.95 5.99C17.56 5.61 17.56 4.98 17.95 4.59L18.34 4.2Z"
+						/><path
+							d="M18.33 19.7L17.94 19.31C17.55 18.92 17.55 18.3 17.95 17.9C18.33 17.52 18.96 17.51 19.35 17.9L19.74 18.29C20.13 18.68 20.13 19.31 19.74 19.7C19.35 20.09 18.72 20.09 18.33 19.7Z"
+						/><path
+							d="M20 11.95V11.94C20 11.39 20.44 10.95 20.99 10.95H22C22.55 10.95 22.99 11.39 22.99 11.94V11.95C22.99 12.5 22.55 12.94 22 12.94H20.99C20.44 12.94 20 12.5 20 11.95Z"
+						/><path
+							fill-rule="evenodd"
+							clip-rule="evenodd"
+							d="M6 11.95C6 8.64 8.69 5.95 12 5.95C15.31 5.95 18 8.64 18 11.95C18 15.26 15.31 17.95 12 17.95C8.69 17.95 6 15.26 6 11.95ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
+						/><path
+							d="M12 22.9H11.99C11.44 22.9 11 22.46 11 21.91V20.95C11 20.4 11.44 19.96 11.99 19.96H12C12.55 19.96 12.99 20.4 12.99 20.95V21.91C12.99 22.46 12.55 22.9 12 22.9Z"
+						/><path
+							d="M5.66 19.69C5.27 20.08 4.64 20.08 4.25 19.69C3.86 19.3 3.86 18.68 4.24 18.28L4.63 17.89C5.02 17.5 5.65 17.5 6.04 17.89L6.05 17.9C6.43 18.28 6.44 18.91 6.05 19.3L5.66 19.69Z"
+						/></svg
+					>
+				</div>
+				<div
+					style="display: flex; align-items: center; justify-content: center; padding-top: 6px; padding-bottom: 6px; color: rgba(0, 0, 0, 0.333);"
+				>
+					<svg
+						viewBox="0 0 24 24"
+						width="18"
+						height="18"
+						fill="currentColor"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							fill-rule="evenodd"
+							clip-rule="evenodd"
+							d="M4.1534 13.6089L4.15362 13.61C4.77322 16.8113 7.42207 19.3677 10.647 19.8853L10.6502 19.8858C13.0412 20.2736 15.2625 19.6103 16.9422 18.2833C11.3549 16.2878 7.9748 10.3524 9.26266 4.48816C5.69846 5.77194 3.35817 9.51245 4.1534 13.6089ZM10.0083 2.21054C4.76622 3.2533 1.09895 8.36947 2.19006 13.9901C2.97006 18.0201 6.28006 21.2101 10.3301 21.8601C13.8512 22.4311 17.0955 21.1608 19.2662 18.8587C19.2765 18.8478 19.2866 18.837 19.2968 18.8261C19.4385 18.6745 19.5757 18.5184 19.7079 18.3581C19.7105 18.355 19.713 18.3519 19.7156 18.3487C19.8853 18.1426 20.0469 17.9295 20.2001 17.7101C20.4101 17.4001 20.2401 16.9601 19.8701 16.9201C19.5114 16.8796 19.1602 16.8209 18.817 16.7452C18.7964 16.7406 18.7758 16.736 18.7552 16.7313C18.6676 16.7114 18.5804 16.6903 18.4938 16.6681C18.4919 16.6676 18.4901 16.6672 18.4882 16.6667C13.0234 15.2647 9.72516 9.48006 11.4542 4.03417C11.4549 4.03214 11.4555 4.03012 11.4562 4.0281C11.4875 3.92954 11.5205 3.83109 11.5552 3.73278C11.5565 3.72911 11.5578 3.72543 11.5591 3.72175C11.6768 3.38921 11.8136 3.05829 11.9701 2.73005C12.1301 2.39005 11.8501 2.01005 11.4701 2.03005C11.1954 2.04379 10.924 2.06848 10.6561 2.10368C10.6517 2.10427 10.6472 2.10486 10.6428 2.10545C10.4413 2.13221 10.2418 2.16492 10.0446 2.2034C10.0325 2.20576 10.0204 2.20814 10.0083 2.21054Z"
+						/></svg
+					>
+				</div>
+			</div>
+		</div>
+		<div class="ant-space-item">
+			<div
+				class="ant-dropdown-trigger"
+				style="height: 45px; border-radius: 2px; padding: 4px 19px 7px; display: flex; flex-direction: column; align-items: center; background: rgb(243, 241, 236); cursor: default; user-select: all;"
+			>
+				<span style="cursor: default; user-select: all; line-height: 22px;">0x283e...11700c</span>
+				<div style="vertical-align: middle; line-height: 1; color: rgba(0, 0, 0, 0.333);">
+					<span style="font-family: sans-serif;">Ξ</span>0
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+
+
+<style>
+	@media (max-width: 767px) {
+		header {
+			display: none !important;
+		}
+	}
+</style>
---
+++ src/lib/components/MobileHeader.svelte
@@ -0,0 +1,62 @@
+<header
+	class="ant-layout-header top-nav top-nav-mobile"
+	style="display: flex; justify-content: space-between; align-items: center; background: var(--background-l0); padding: 16px 8px; width: 100%; position: fixed;"
+>
+	<div class="ant-collapse ant-collapse-icon-position-left" style="border: none;">
+		<div class="ant-collapse-item ant-collapse-no-arrow" style="border: none;">
+			<div class="ant-collapse-header" role="button" tabindex="0" aria-expanded="false">
+				<div class="ant-space ant-space-horizontal ant-space-align-center" style="gap: 8px;">
+					<div class="ant-space-item" style="">
+						<a href="/" style="display: inline-block;"
+							><img src="/juice_logo-ol.png" alt="Juicebox logo" style="height: 30px;" /></a
+						>
+					</div>
+					<div class="ant-space-item">
+						<span
+							role="img"
+							aria-label="menu"
+							class="anticon anticon-menu"
+							style="color: rgba(0, 0, 0, 0.933); font-size: 20px; padding-top: 6px; padding-left: 10px;"
+							><svg
+								viewBox="64 64 896 896"
+								focusable="false"
+								data-icon="menu"
+								width="1em"
+								height="1em"
+								fill="currentColor"
+								aria-hidden="true"
+								><path
+									d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"
+								/></svg
+							></span
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+<div class="padding"></div>
+
+<style>
+    header {
+        z-index: 1000;
+    }
+	@media (min-width: 766.99px) {
+		header {
+			display: none !important;
+		}
+        .padding {
+            display: none;
+        }
+	}
+    .padding {
+        height: 64px;
+        width: 500px;
+        position: relative;
+        display: flex;
+    }
+	.ant-collapse {
+		background: var(--background-l0);
+	}
+</style>
