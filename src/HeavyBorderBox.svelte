b4ca144f57c8e35f0db369211c4298b7cb2784b6
Evie <evita@stenqvist.co>
2022-04-29 14:55:03 +0200

    Chunky border box component

---
+++ src/HeavyBorderBox.svelte
@@ -0,0 +1,15 @@
+<section>
+  <slot />
+</section>
+
+<style>
+  section {
+    padding: 2rem;
+    margin-bottom: 10px;
+    background: rgb(243, 241, 236);
+    box-shadow: rgb(231 227 220) 10px 10px;
+    border-radius: 1px;
+    stroke: none;
+    color: rgba(0, 0, 0, 0.933);
+  }
+</style>
