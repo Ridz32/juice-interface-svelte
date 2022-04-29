6c78bf13b7438741008385dd87713321ee02b01c
Evie <evita@stenqvist.co>
2022-04-29 11:25:09 +0200

    :lipstick: InfoBox

--- src/InfoBox.svelte
+++ src/InfoBox.svelte
@@ -3,15 +3,15 @@
 </script>
 
 <aside>
-  <p>
-    <!-- The round info circle -->
+  <!-- The round info circle -->
+  <!-- TODO put all svg icons in own component -->
+  <div>
     <svg
       viewBox="64 64 896 896"
       focusable="false"
       data-icon="info-circle"
       width="1em"
       height="1em"
-      fill="currentColor"
       aria-hidden="true"
       ><path
         d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"
@@ -19,14 +19,28 @@
         d="M464 336a48 48 0 1096 0 48 48 0 10-96 0zm72 112h-48c-4.4 0-8 3.6-8 8v272c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V456c0-4.4-3.6-8-8-8z"
       /></svg
     >
-    {info}
+  </div>
+  <p>
+    {@html info}
   </p>
 </aside>
 
 <style>
   aside {
     background-color: rgb(231, 227, 220);
-    padding: 5px 20px;
+    padding: 20px 20px;
     font-weight: 300;
+    display: flex;
+    align-items: flex-start;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  svg {
+    margin-right: 10px;
+    width: 18px;
+    height: 18px;
   }
 </style>
