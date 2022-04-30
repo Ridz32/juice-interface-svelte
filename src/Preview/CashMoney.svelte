3dcc1b7fd2e43f97e5504166ea8cc331cfd3a85e
Evie <evita@stenqvist.co>
2022-04-30 17:14:33 +0200

    Funding info with tooltips on hover

--- src/Preview/CashMoney.svelte
+++ src/Preview/CashMoney.svelte
@@ -1,31 +1,55 @@
 <script lang="ts">
+  import Icon from "../Icon.svelte";
   import InfoSpaceBetween from "../InfoSpaceBetween.svelte";
+  import Popover from "../Popover.svelte";
 </script>
 
 <!-- TODO: rename this component, quite undescriptive given other sibling is "Funding" -->
 <InfoSpaceBetween>
-  <!-- TODO info question mark thingy with popover on hover-->
-  <p slot="left">In juicebox</p>
+  <div slot="left">
+    <p>In juicebox</p>
+    <Popover message="The balance of this project in the Juicebox contract."
+      ><Icon name="questionCircle" /></Popover
+    >
+  </div>
   <p slot="right" class="money"><span>Ξ</span>0</p>
 </InfoSpaceBetween>
 <InfoSpaceBetween>
-  <!-- TODO info question mark thingy with popover on hover-->
-  <p slot="left">Distributed</p>
-  <p slot="right">100% Overflow</p>
+  <div slot="left">
+    <p>Distributed</p>
+    <Popover message="The amount that has been distributed from the Juicebox balance in this funding cycle, out of the current distribution limit. No more than the distribution limit can be distributed in a single funding cycle—any remaining ETH in Juicebox is overflow, until the next cycle begins."
+      ><Icon name="questionCircle" /></Popover
+    >
+  </div>
+  <!-- <p slot="left">Distributed</p> -->
+  <!-- <p slot="right">100% Overflow</p> -->
+  <div slot="right">
+    <p>100% Overflow</p>
+    <Popover message="The target for this funding cycle is 0, meaning all funds in Juicebox are currently considered overflow. Overflow can be redeemed by token holders, but not distributed."
+      ><Icon name="questionCircle" /></Popover
+    >
+  </div>
 </InfoSpaceBetween>
 <!-- TODO the overflow progress bar showing % of funding? -->
 <InfoSpaceBetween>
-    <!-- TODO info question mark thingy with popover on hover-->
-    <p slot="left">In wallet</p>
-  </InfoSpaceBetween>
+  <div slot="left">
+    <p>In wallet</p>
+    <Popover message="The balance of the project owner's wallet."
+      ><Icon name="questionCircle" /></Popover
+    >
+  </div>
+</InfoSpaceBetween>
 
 <style>
   p {
     font-size: 0.8rem;
     text-transform: uppercase;
-    color: rgba(0, 0, 0, 0.333);
     font-weight: 500;
     margin: 5px 0px;
+    display: inline-block;
+  }
+  div[slot="left"], div[slot="right"], p {
+    color: rgba(0, 0, 0, 0.333);
   }
   span {
     font-family: sans-serif;
