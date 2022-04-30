440dca1056260a8bb39aeb45896bf98bb5f44107
Evie <evita@stenqvist.co>
2022-04-30 21:26:48 +0200

    Collapsible section component

---
+++ src/CollapsibleSection.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Icon from "./Icon.svelte";
+  let expanded = true;
+</script>
+
+<button aria-expanded={expanded} on:click={() => (expanded = !expanded)}>
+  <Icon name="caret" direction={expanded ? "e" : "n"} />
+  <slot name="header" />
+</button>
+
+{#if expanded}
+  <slot />
+{/if}
+
+<style>
+  button {
+    background-color: unset;
+    border: none;
+    border-bottom: 1px solid var(--light-body);
+    color: var(--text-header);
+    display: inline-flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin: 0;
+  }
+</style>
