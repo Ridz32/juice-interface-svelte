7079ec13a59aad82feb99e84acc88abb9789308c
Evie <evita@stenqvist.co>
2022-05-01 16:24:19 +0200

    Borrowed utility code from OG jb repo

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import svelte from 'rollup-plugin-svelte';
+import alias from '@rollup/plugin-alias';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
@@ -45,6 +46,13 @@ export default {
 				// enable run-time checks when not in production
 				dev: !production
 			}
+		}),
+		alias({
+			entries: [
+				{find: 'utils', replacement: 'src/utils'},
+				{find: 'components', replacement: 'src/components'},
+				{find: 'constants', replacement: 'src/constants'}
+			]
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
---
+++ src/constants/numbers.ts
@@ -0,0 +1,9 @@
+/**
+ * Taken from jb-interface: src/constants/numbers.ts
+ * Date: 2022-05-01
+ */
+
+export const WAD_DECIMALS = 18
+
+export const SECONDS_IN_DAY = 24 * 60 * 60
+export const SECONDS_IN_HOUR = 60 * 60
