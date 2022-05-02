cd319224ffd130674078047ebc958467948a7ea7
Evie <evita@stenqvist.co>
2022-05-02 16:23:37 +0200

    :WIP: Store reflects whole project model

--- src/lib/create/stores.ts
+++ src/lib/create/stores.ts
@@ -1,4 +1,14 @@
 import { writable, derived } from "svelte/store";
+import { BigNumber } from '@ethersproject/bignumber';
+import * as constants from '@ethersproject/constants'
+import type { V2BallotState } from '$models/ballot';
+import type { ProjectMetadataV4 } from '$models/project-metadata';
+import type { V2FundingCycle, V2FundingCycleData, V2FundingCycleMetadata } from '$models/v2/fundingCycle';
+import type { Split } from '$models/v2/splits';
+import { connectedAccount } from "$stores/web3";
+import { V2_CURRENCY_ETH } from "$utils/v2/currency";
+import { DEFAULT_ISSUANCE_RATE, redemptionRateFrom } from '$utils/v2/math'
+import Store from "$utils/Store";
 
 export const projectDetails = writable({
   name: null,
@@ -19,23 +29,6 @@ export const visitedFundingDrawers = writable({
 
 export const modal = writable({});
 
-export enum DistributionLimit {
-  NOT_SET = "NOT_SET",
-  ZERO = "ZERO", // no funds can be distributed, TODO set when funding drawer been open (i.e. default)
-  NO_LIMIT = "NO_LIMIT", // "Infinite" funds can be distributed as much as they want
-  TARGET = "TARGET", // "Specific limit" funds can be distributed to a target amount
-}
-
-// TODO when funding cycle duration is not set,
-// an info message appears in the details dropdown in Preview
-
-export const splits = writable([]);
-
-export const fundingDetails = writable({
-  distributionLimit: DistributionLimit.NOT_SET,
-  duration: null,
-})
-
 export const isReviewPanel = writable(false);
 
 export const twitterHandle = derived(
@@ -43,3 +36,170 @@ export const twitterHandle = derived(
   // TODO: Check if it starts with @, if not prefix it
   ($twitter) => `@${$twitter}!`
 );
+
+type V2ProjectLoadingStates = {
+  ETHBalanceLoading: boolean
+  balanceInDistributionLimitCurrencyLoading: boolean
+  distributionLimitLoading: boolean
+  fundingCycleLoading: boolean
+  usedDistributionLimitLoading: boolean
+}
+
+export type V2ProjectContextType = {
+  isPreviewMode?: boolean
+
+  projectId: BigNumber | undefined
+  projectMetadata: ProjectMetadataV4 | undefined
+  tokenAddress: string | undefined
+  tokenSymbol: string | undefined
+  terminals: string[] | undefined // array of terminal addresses, 0xABC...
+  primaryTerminal: string | undefined
+  ETHBalance: BigNumber | undefined
+  projectOwnerAddress: string | undefined
+  balanceInDistributionLimitCurrency: BigNumber | undefined
+  usedDistributionLimit: BigNumber | undefined // how much has been distributed
+
+  fundingCycleMetadata: V2FundingCycleMetadata | undefined
+  fundingCycle: V2FundingCycle | undefined
+  ballotState: V2BallotState | undefined
+
+  distributionLimit: BigNumber | undefined
+  distributionLimitCurrency: BigNumber | undefined
+
+  payoutSplits: Split[] | undefined
+  reservedTokensSplits: Split[] | undefined
+
+  primaryTerminalCurrentOverflow: BigNumber | undefined
+  totalTokenSupply: BigNumber | undefined
+
+  loading: V2ProjectLoadingStates
+}
+
+const fundingCycleData: V2FundingCycleData = {
+  duration: BigNumber.from(0),
+  weight: BigNumber.from(100),
+  discountRate: BigNumber.from(0),
+  // TODO ballot, look at hooks/v2/V2ContractLoader.ts for more info
+  // ballot: contracts?.JBETHPaymentTerminal.address ?? '', // hex, contract address
+  ballot: ""
+}
+
+const fundingCycle: V2FundingCycle = {
+  ...fundingCycleData,
+  number: BigNumber.from(1),
+  configuration: BigNumber.from(0),
+  basedOn: BigNumber.from(0),
+  start: BigNumber.from(Date.now()).div(1000),
+  metadata: BigNumber.from(0),
+}
+
+export const projectMetadata = new Store<ProjectMetadataV4>({
+  version: 4,
+  name: "",
+  description: "",
+  infoUri: "",
+  logoUri: "",
+  twitter: "",
+  discord: "",
+  tokens: [],
+  payButton: "Pay",
+  payDisclosure: "",
+})
+
+const fundingCycleMetadata: V2FundingCycleMetadata = {
+  reservedRate: BigNumber.from(0), // A number from 0-10,000
+  redemptionRate: redemptionRateFrom('100'), // A number from 0-10,000
+  ballotRedemptionRate: redemptionRateFrom('100'), // A number from 0-10,000
+  pausePay: false,
+  pauseDistributions: false,
+  pauseRedeem: false,
+  allowMinting: false,
+  pauseBurn: false,
+  allowChangeToken: false,
+  allowTerminalMigration: false,
+  allowControllerMigration: false,
+  allowSetTerminals: false,
+  allowSetController: false,
+  holdFees: false,
+  useTotalOverflowForRedemptions: false,
+  useDataSourceForPay: false,
+  useDataSourceForRedeem: false,
+  dataSource: constants.AddressZero,
+  // isPreviewMode: true,
+}
+
+export const payoutSplits = new Store<[Split] | []>([]);
+export const reservedTokensSplits = new Store<[Split] | []>([]);
+
+const project: V2ProjectContextType = {
+  isPreviewMode: true,
+
+  projectId: BigNumber.from(0),
+  projectMetadata: projectMetadata.get(),
+
+  fundingCycle,
+  fundingCycleMetadata,
+
+  distributionLimit: undefined,
+  distributionLimitCurrency: BigNumber.from(V2_CURRENCY_ETH),
+
+  payoutSplits: payoutSplits.get(),
+  reservedTokensSplits: reservedTokensSplits.get(),
+
+  usedDistributionLimit: BigNumber.from(0),
+  ETHBalance: BigNumber.from(0),
+  balanceInDistributionLimitCurrency: BigNumber.from(0),
+
+  tokenAddress: undefined,
+  terminals: [],
+  primaryTerminal: undefined,
+  tokenSymbol: undefined,
+  projectOwnerAddress: connectedAccount.get(),
+  ballotState: undefined,
+  primaryTerminalCurrentOverflow: undefined,
+  totalTokenSupply: undefined,
+
+  loading: {
+    ETHBalanceLoading: false,
+    balanceInDistributionLimitCurrencyLoading: false,
+    distributionLimitLoading: false,
+    fundingCycleLoading: false,
+    usedDistributionLimitLoading: false,
+  },
+}
+
+// export const V2ProjectContext = writable<V2ProjectContextType>({
+//   isPreviewMode: false,
+
+//   projectId: undefined,
+//   projectMetadata: undefined,
+//   tokenAddress: undefined,
+//   tokenSymbol: undefined,
+//   terminals: undefined,
+//   primaryTerminal: undefined,
+//   ETHBalance: undefined,
+//   projectOwnerAddress: undefined,
+//   balanceInDistributionLimitCurrency: undefined,
+//   usedDistributionLimit: undefined,
+
+//   fundingCycleMetadata: undefined,
+//   fundingCycle: undefined,
+//   ballotState: undefined,
+
+//   distributionLimit: undefined,
+//   distributionLimitCurrency: undefined,
+
+//   payoutSplits: undefined,
+//   reservedTokensSplits: undefined,
+
+//   primaryTerminalCurrentOverflow: undefined,
+//   totalTokenSupply: undefined,
+
+//   loading: {
+//     ETHBalanceLoading: false,
+//     balanceInDistributionLimitCurrencyLoading: false,
+//     distributionLimitLoading: false,
+//     fundingCycleLoading: false,
+//     usedDistributionLimitLoading: false,
+//   },
+// })
