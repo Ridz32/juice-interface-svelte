bae776095270d6fe39fab7364a376f72db552df4
Evie <evita@stenqvist.co>
2022-05-03 16:11:43 +0200

    Money component that handles ETH/USD

---
+++ src/lib/components/Money.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { BigNumber } from "ethers";
+    import { Currency } from '$constants';
+	import Ethereum from '$lib/create/Ethereum.svelte';
+	import USD from '$lib/components/USD.svelte';
+
+	export let currency: Currency;
+	export let amount = BigNumber.from('0');
+</script>
+
+{#if currency === Currency.ETH}
+	<Ethereum />{amount}
+{:else}
+	<USD />{amount}
+{/if}
