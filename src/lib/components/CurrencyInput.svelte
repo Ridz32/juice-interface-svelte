ae47970fd2584e387e3826783223c227c0dbc8fb
Evie <evita@stenqvist.co>
2022-05-03 12:56:57 +0200

    CurrencyInput as standalone component

---
+++ src/lib/components/CurrencyInput.svelte
@@ -0,0 +1,109 @@
+<script context="module" lang="ts">
+	export enum Currency {
+		ETH = 'ETH',
+		USD = 'USD'
+	}
+</script>
+
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+    import { BigNumber } from 'ethers';
+	import Icon from '$lib/components/Icon.svelte';
+
+
+	export let currency: Currency = Currency.ETH;
+    export let inputValue = 0;
+
+	const dispatch = createEventDispatcher();
+
+    /**
+     * onBlur dispatches "setValue" event which handles BigNumber conversion
+     * bind to value directly to get the input without conversion
+     * @param e: Event
+     * @dispatch setValue
+     */
+    function onBlur(e: Event) {
+        const target = e.target as HTMLInputElement;
+        const value = BigNumber.from(target.value);
+        dispatch('setValue', { value });
+    }
+
+	function onClick() {
+        currency = currency === Currency.ETH ? Currency.USD : Currency.ETH;
+		dispatch('switchCurrency', { currency });
+	}
+</script>
+
+<div class="input-container">
+	<input placeholder="0" bind:value={inputValue} on:blur={onBlur} />
+	<div role="button" class="currency" on:click={onClick}>
+		{currency}
+		<Icon name="caret" direction="e" />
+	</div>
+</div>
+
+<style>
+	.input-container {
+		flex: 1;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+
+		box-sizing: border-box;
+		border: 1px solid #d9d9d9;
+		border-radius: 2px;
+        width: 100%;
+	}
+	/* TODO we want the parent to be focused when input is... */
+	input:focus-visible {
+		outline: none;
+	}
+
+	/* TODO input asbtraction, this is semi borrowed from FormField */
+	input {
+		flex-grow: 1;
+		margin: 0;
+		font-variant: tabular-nums;
+		font-family: inherit;
+		font-size: 14px;
+		font-weight: 300;
+		list-style: none;
+		-webkit-font-feature-settings: 'tnum', 'tnum';
+		font-feature-settings: 'tnum', 'tnum';
+		position: relative;
+		display: inline-block;
+		min-width: 0;
+		padding: 4px 11px;
+		color: rgba(0, 0, 0, 0.85);
+		line-height: 1.5715;
+		background-color: transparent;
+		border: none;
+		transition: all 0.3s;
+	}
+
+	.input-container:focus-visible {
+		border: 1px solid #3f51b5;
+	}
+
+	.currency {
+		cursor: pointer;
+		color: rgb(24, 180, 199);
+		background: rgba(50, 200, 219, 0.267);
+		font-size: 12px;
+		white-space: nowrap;
+		padding: 1px 6px;
+		margin-left: 0px;
+		margin-right: 2px;
+		border-radius: 1px;
+
+		display: flex;
+		align-items: center;
+	}
+
+	:global(.currency svg) {
+		margin-left: 4px;
+		transform: translate(0, 1px) rotate(90deg) !important;
+		width: 0.6em;
+		height: 0.6em;
+	}
+</style>
--- src/lib/create/Preview/Pay.svelte
+++ src/lib/create/Preview/Pay.svelte
@@ -1,36 +1,26 @@
 <script lang="ts">
 	import { projectMetadata } from '../stores';
 	import Button from '../../components/Button.svelte';
-	import Icon from '$lib/components/Icon.svelte';
 	import ETH from '../Ethereum.svelte';
-
-	enum Currency {
-		ETH = 'ETH',
-		USD = 'USD'
-	}
+	import CurrencyInput, { Currency } from '$lib/components/CurrencyInput.svelte';
+	import { BigNumber } from 'ethers';
 
 	let currency = Currency.ETH;
 	let receiveText = 'Receive 1,000,000 tokens/1 ETH';
-	function switchCurrency() {
-		if (currency === Currency.ETH) {
-			currency = Currency.USD;
-			receiveText = 'Receive 357 tokens/1 USD';
-		} else {
-			currency = Currency.ETH;
-			receiveText = 'Receive 1,000,000 tokens/1 ETH';
-		}
+	let amount: BigNumber = BigNumber.from('0');
+
+	// TODO dollar value should be calculated from ETH conversion rate
+	$: receiveText =
+		currency === Currency.ETH ? 'Receive 1,000,000 tokens/1 ETH' : 'Receive 357 tokens/1 USD';
+
+	function setValue(value) {
+		amount = value;
 	}
 </script>
 
 <div class="wrapper">
 	<div class="stacked expand">
-		<div class="input-container">
-			<input placeholder="0" />
-			<div role="button" class="currency" on:click={switchCurrency}>
-				{currency}
-				<Icon name="caret" direction="e" />
-			</div>
-		</div>
+		<CurrencyInput bind:currency on:setValue={setValue} />
 		<small>{receiveText}</small>
 	</div>
 	<div class="stacked">
@@ -47,51 +37,9 @@
 		display: flex;
 	}
 
-	/* TODO input asbtraction, this is semi borrowed from FormField */
-	input {
-		flex-grow: 1;
-		margin: 0;
-		font-variant: tabular-nums;
-		font-family: inherit;
-		font-size: 14px;
-		font-weight: 300;
-		list-style: none;
-		-webkit-font-feature-settings: 'tnum', 'tnum';
-		font-feature-settings: 'tnum', 'tnum';
-		position: relative;
-		display: inline-block;
-		min-width: 0;
-		padding: 4px 11px;
-		color: rgba(0, 0, 0, 0.85);
-		line-height: 1.5715;
-		background-color: transparent;
-		border: none;
-		transition: all 0.3s;
-	}
-
-	.input-container {
-		flex: 1;
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-
-		box-sizing: border-box;
-		border: 1px solid #d9d9d9;
-		border-radius: 2px;
-
-		margin-right: 10px;
-	}
-	/* TODO we want the parent to be focused when input is... */
-	input:focus-visible {
-		outline: none;
-	}
-
-	.input-container:focus-visible {
-		border: 1px solid #3f51b5;
-	}
-
 	.expand {
 		flex: 1;
+		margin-right: 10px;
 	}
 
 	.stacked {
@@ -101,27 +49,5 @@
 
 	.stacked:last-of-type {
 		text-align: center;
-	}
-
-	.currency {
-		cursor: pointer;
-		color: rgb(24, 180, 199);
-		background: rgba(50, 200, 219, 0.267);
-		font-size: 12px;
-		white-space: nowrap;
-		padding: 1px 6px;
-		margin-left: 0px;
-		margin-right: 2px;
-		border-radius: 1px;
-
-		display: flex;
-		align-items: center;
-	}
-
-	:global(.currency svg) {
-		margin-left: 4px;
-		transform: translate(0, 1px) rotate(90deg) !important;
-		width: 0.6em;
-		height: 0.6em;
 	}
 </style>
