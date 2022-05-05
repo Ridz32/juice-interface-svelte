91626fc34693b0a4b0800f30b6a5bc77a0b1f28f
Evie <evita@stenqvist.co>
2022-05-05 13:49:40 +0200

    Funding cycle data in Preview pane

--- src/lib/create/Preview/Main.svelte
+++ src/lib/create/Preview/Main.svelte
@@ -1,33 +1,46 @@
 <script lang="ts">
-	import { BigNumber } from '@ethersproject/bignumber';
+	import type { BigNumber } from '@ethersproject/bignumber';
 	import CashMoney from './CashMoney.svelte';
 	import Funding from './Funding.svelte';
 	import InfoHeader from './InfoHeader.svelte';
 	import Pay from './Pay.svelte';
 
 	import { fundingCycle } from '../stores';
+	import {
+		getUnsafeV2FundingCycleProperties,
+		V2FundingCycleRiskCount
+	} from '$utils/v2/fundingCycle';
 
-	// FUNDING CYCLE MOCK DATA WHILE REBUILDUING
+	function getFundingFields(fundingCycle) {
+		return {
+			fundingCycleNumber: fundingCycle.number,
+			fundingCycleStartTime: fundingCycle.start,
+			fundingCycleDurationSeconds: fundingCycle.duration,
+			fundingCycleRiskCount: V2FundingCycleRiskCount(fundingCycle),
+			fundingCycleRiskProperties: getUnsafeV2FundingCycleProperties(fundingCycle),
+			isFundingCycleRecurring: true,
+			isPreviewMode: true
+		};
+	}
 
-	// $: {
-	//   fundingCycleDuration
-	// }
-
-	const mockDataFunding = {
-		fundingCycleNumber: BigNumber.from(1),
-		fundingCycleStartTime: BigNumber.from(1588294400),
-		fundingCycleDurationSeconds: $fundingCycle.duration,
-		fundingCycleRiskCount: 1,
-		isFundingCycleRecurring: true,
-		isPreviewMode: true
+	let fundingFields: svelte.JSX.IntrinsicAttributes & {
+		fundingCycleNumber: BigNumber;
+		fundingCycleStartTime: BigNumber;
+		fundingCycleDurationSeconds: BigNumber;
+		fundingCycleRiskCount: number;
+		fundingCycleRiskProperties: any;
+		isFundingCycleRecurring: boolean;
+		isPreviewMode: boolean;
 	};
+
+	$: fundingFields = getFundingFields($fundingCycle);
 </script>
 
 <h1 class="title">Preview:</h1>
 <InfoHeader />
 <CashMoney />
 <Pay />
-<Funding {...{ ...mockDataFunding, fundingCycleDurationSeconds: $fundingCycle.duration }} />
+<Funding {...fundingFields} />
 
 <style>
 	.title {
