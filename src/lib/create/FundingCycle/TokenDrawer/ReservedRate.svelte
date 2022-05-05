4e2163522943c50a486a85261c194b38c9530a6c
Evie <evita@stenqvist.co>
2022-05-05 19:31:04 +0200

    Reserved rate token for contributors and project info

--- src/lib/create/FundingCycle/TokenDrawer/ReservedRate.svelte
+++ src/lib/create/FundingCycle/TokenDrawer/ReservedRate.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import type { BigNumberish } from 'ethers';
 	import { DEFAULT_ISSUANCE_RATE } from '$utils/v2/math';
 	import { formattedNum } from '$utils/formatNumber';
 	import Toggle from '$lib/components/Toggle.svelte';
@@ -6,25 +7,40 @@
 	import Range from '$lib/components/Range.svelte';
 
 	export let checked: boolean = false;
+	export let reservedRate: number = 0;
 
-    export let value = 0;
+	let rangeValue: number[] = [reservedRate];
 
-    // Reserved tokens received by project per ETH
-	const initialReservedTokensPerEth = DEFAULT_ISSUANCE_RATE * ((value ?? 0) / 100);
+	// Reserved tokens received by project per ETH
+	let initialReservedTokensPerEth: BigNumberish;
 	// Tokens received by contributor's per ETH
-	const initialIssuanceRate = DEFAULT_ISSUANCE_RATE - initialReservedTokensPerEth;
+	let initialIssuanceRate: BigNumberish;
+	$: {
+		reservedRate = rangeValue[0];
+		initialReservedTokensPerEth = DEFAULT_ISSUANCE_RATE * ((reservedRate ?? 0) / 100);
+		initialIssuanceRate = DEFAULT_ISSUANCE_RATE - initialReservedTokensPerEth;
+	}
 </script>
 
 <header>
-	<Toggle id="reserved" bind:checked><h3>Reserved rate</h3></Toggle>
+	<Toggle id="reserved" bind:checked
+		><h3>
+			Reserved rate
+			{#if !reservedRate}
+				<span>(0%)</span>
+			{/if}
+		</h3></Toggle
+	>
 </header>
 {#if checked}
-	<Range />
+	<Range bind:values={rangeValue} />
 {/if}
 <InfoBox
 	info={`Initial issuance rate will be ${formattedNum(
 		initialIssuanceRate
-	)} tokens / ETH for contributors. 0 tokens / ETH will be reserved by the project.`}
+	)} tokens / ETH for contributors. ${formattedNum(
+		initialReservedTokensPerEth
+	)} tokens / ETH will be reserved by the project.`}
 />
 <br />
 <p>
