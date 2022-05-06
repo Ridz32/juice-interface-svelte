02e5e8d0acfd653de68f666b8b59e378fab61e64
Evie <evita@stenqvist.co>
2022-05-06 11:21:03 +0200

    [UI] Ballot strategies

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,32 @@
+<!-- <script lang="ts">
+    import type { SvelteInputProps } from "svelte";
+
+    export let props: SvelteInputProps;
+</script> -->
+
+<input {...$$props} />
+
+<style>
+	input {
+		width: 100%;
+		background: transparent;
+		border: 1px solid var(--stroke-tertiary);
+		transition: border-color 120ms ease-out;
+		text-indent: 5px;
+		padding: 2.5px;
+	}
+
+	input::placeholder {
+		color: var(--text-tertiary);
+		font-weight: 300;
+	}
+
+	input:focus {
+		border: var(--background-action-primary) 1px solid;
+		outline: none;
+	}
+
+	input:hover {
+		border: var(--background-action-primary) 1px solid;
+	}
+</style>
--- src/lib/create/RulesDrawer.svelte
+++ src/lib/create/RulesDrawer.svelte
@@ -1,13 +1,22 @@
+<script context="module">
+	import { ballotStrategies, DEFAULT_BALLOT_STRATEGY } from '$constants/v2/ballotStrategies';
+	const strategies = ballotStrategies();
+</script>
+
 <script>
+	import { constants } from 'ethers';
 	import Button from '$lib/components/Button.svelte';
+	import ExternalLink from '$lib/components/ExternalLink.svelte';
 	import HeavyBorderBox from '$lib/components/HeavyBorderBox.svelte';
 	import Icon from '$lib/components/Icon.svelte';
+	import Input from '$lib/components/Input.svelte';
 	import Toggle from '$lib/components/Toggle.svelte';
+	import ReconBox from './FundingCycle/TokenDrawer/ReconBox.svelte';
 	import { fundingCycleMetadata } from './stores';
 
 	let pausePay = $fundingCycleMetadata.pausePay;
 	let allowMinting = $fundingCycleMetadata.allowMinting;
-    export let close;
+	export let close;
 
 	function onSaveRules() {
 		fundingCycleMetadata.update((fcm) => ({
@@ -38,6 +47,30 @@
 </HeavyBorderBox>
 <HeavyBorderBox>
 	<h4>Reconfiguration</h4>
+	{#each strategies as strategy}
+		<ReconBox>
+			<h3 slot="header">{strategy.name}</h3>
+			<p slot="body">{strategy.description}</p>
+			<small slot="address">Contract address: {strategy.address}</small>
+		</ReconBox>
+	{/each}
+	<ReconBox>
+		<!-- TODO Rinkeby should be from signerNetwork -->
+		<h3 slot="header">Custom strategy</h3>
+		<div slot="body">
+			<div class="input">
+				<Input placeholder={constants.AddressZero} />
+			</div>
+			<p>
+				The address of any smart contract deployed on Rinkeby that implements
+				<ExternalLink
+					href="https://github.com/jbx-protocol/juice-contracts-v1/blob/main/contracts/FundingCycles.sol"
+				>
+					this interface.
+				</ExternalLink>
+			</p>
+		</div>
+	</ReconBox>
 </HeavyBorderBox>
 <Button onClick={onSaveRules}>Save rules</Button>
 
@@ -57,5 +90,19 @@
 
 	.option {
 		margin-bottom: 20px;
+	}
+
+	[slot='body'],
+	[slot='body'] p {
+		color: var(--text-primary);
+	}
+
+	[slot='address'] {
+		color: var(--text-tertiary);
+		font-weight: 300;
+	}
+
+	.input {
+		width: 400px;
 	}
 </style>
