cad772a0b4b1a18fb9061b91289d195c454193df
Evie <evita@stenqvist.co>
2022-05-06 15:31:44 +0200

    Make linter happy

--- src/lib/create/Preview/Main.svelte
+++ src/lib/create/Preview/Main.svelte
@@ -1,46 +1,15 @@
 <script lang="ts">
-	import type { BigNumber } from '@ethersproject/bignumber';
 	import CashMoney from './CashMoney.svelte';
 	import Funding from './Funding.svelte';
 	import InfoHeader from './InfoHeader.svelte';
 	import Pay from './Pay.svelte';
-
-	import { fundingCycle } from '../stores';
-	import {
-		getUnsafeV2FundingCycleProperties,
-		V2FundingCycleRiskCount
-	} from '$utils/v2/fundingCycle';
-
-	function getFundingFields(fundingCycle) {
-		return {
-			fundingCycleNumber: fundingCycle.number,
-			fundingCycleStartTime: fundingCycle.start,
-			fundingCycleDurationSeconds: fundingCycle.duration,
-			fundingCycleRiskCount: V2FundingCycleRiskCount(fundingCycle),
-			fundingCycleRiskProperties: getUnsafeV2FundingCycleProperties(fundingCycle),
-			isFundingCycleRecurring: true,
-			isPreviewMode: true
-		};
-	}
-
-	let fundingFields: svelte.JSX.IntrinsicAttributes & {
-		fundingCycleNumber: BigNumber;
-		fundingCycleStartTime: BigNumber;
-		fundingCycleDurationSeconds: BigNumber;
-		fundingCycleRiskCount: number;
-		fundingCycleRiskProperties: any;
-		isFundingCycleRecurring: boolean;
-		isPreviewMode: boolean;
-	};
-
-	$: fundingFields = getFundingFields($fundingCycle);
 </script>
 
 <h1 class="title">Preview:</h1>
 <InfoHeader />
 <CashMoney />
 <Pay />
-<Funding {...fundingFields} />
+<Funding />
 
 <style>
 	.title {
