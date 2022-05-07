dee30361fefb36da0e1cf3f014cc533b332684e7
Evie <evita@stenqvist.co>
2022-05-07 18:55:07 +0200

    Don't store whole graph url in enviroment file, only API_KEY and ID

--- src/utils/graph.ts
+++ src/utils/graph.ts
@@ -186,7 +186,7 @@ export const formatGraphQuery = <E extends EntityKey, K extends EntityKeys<E>>(
 	)} } }`;
 };
 
-const subgraphUrl = import.meta.env.VITE_SUBGRAPH_URL as string;
+const subgraphUrl = `https://gateway.thegraph.com/api/${import.meta.env.VITE_SUBGRAPH_API_KEY}/subgraphs/id/${import.meta.env.VITE_SUBGRAPH_ID}`;
 
 export const trimHexZero = (hexStr: string) => hexStr.replace('0x0', '0x');
 
