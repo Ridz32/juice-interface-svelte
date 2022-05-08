17741746bdbc6b92735248150564eedd4a311319
EVMCompatible <evmcompatible@gmail.com>
2022-05-08 23:32:54 +0500

    persist dark mode

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,10 @@
+import { browser } from '$app/env';
 import Store from '$utils/Store';
 
 const data = new Store();
 
-export const darkMode = new Store(false);
+export const darkMode = new Store(browser ? localStorage.getItem('DARK_MODE') === 'true' : false);
+
+if (browser) {
+	darkMode.subscribe((value) => localStorage.setItem('DARK_MODE', JSON.stringify(value)));
+}
