c5c61e658bc710f904a1351672f9864164794e8c
Evie <evita@stenqvist.co>
2022-05-08 19:45:06 +0200

    [ui] Activity header Pay/Redeem/etc

--- src/lib/project/Activity.svelte
+++ src/lib/project/Activity.svelte
@@ -5,6 +5,14 @@
 	import type { ProjectMetadata } from '$models/project-metadata';
 	import Icon from '$lib/components/Icon.svelte';
 	import PayEvent from './PayEvent.svelte';
+	import InfoSpaceBetween from '$lib/components/InfoSpaceBetween.svelte';
+
+	enum ActivityType {
+		PAY = 'PAY',
+		REDEEM = 'REDEEM',
+		WITHDRAW = 'WITHDRAW',
+		RESERVES = 'RESERVES'
+	}
 
 	const projectContext = getContext('PROJECT') as {
 		project: Store<Project>;
@@ -12,15 +20,28 @@
 	};
 	const project = projectContext.project;
 	const payEvents = $project.payEvents;
-	console.log(payEvents);
+
 	export let loading: boolean;
+	export let current: ActivityType = ActivityType.PAY;
 </script>
 
-<!-- {#each projectCategories as category}
-	<p>{category}</p>
-{/each} -->
-
 <section>
+	<header>
+		<InfoSpaceBetween>
+			<div slot="left">
+				<h4>Activity</h4>
+				<div class="icon">
+					<Icon name="download" />
+				</div>
+			</div>
+			<div slot="right">
+				<p class:current={ActivityType.PAY === current}>Pay</p>
+				<p class:current={ActivityType.REDEEM === current}>Redeem</p>
+				<p class:current={ActivityType.WITHDRAW === current}>Withdraw</p>
+				<p class:current={ActivityType.RESERVES === current}>Reserves</p>
+			</div>
+		</InfoSpaceBetween>
+	</header>
 	{#if loading}
 		<Icon name="loading" spin />
 	{/if}
@@ -35,8 +56,40 @@
 		width: 100%;
 		padding-left: 20px;
 		padding-right: 20px;
-		margin-top: 40px;
 		margin: 0 auto;
+		margin-top: 40px;
+	}
+
+	h4 {
+		font-weight: 600;
+		color: var(--text-header);
+	}
+
+	div[slot='left'],
+	div[slot='right'] {
+		display: flex;
+		gap: 20px;
+	}
+
+	div[slot='left'] {
+		display: flex;
+		align-items: baseline;
+	}
+
+	div[slot='right'] {
+		text-transform: uppercase;
+		font-weight: 300;
+		color: var(--text-tertiary);
+	}
+
+	.current {
+		font-weight: 600;
+		color: var(--text-secondary);
+	}
+
+	.icon {
+		font-size: 1rem;
+		color: var(--text-action-primary);
 	}
 
 	@media (max-width: 768px) {
