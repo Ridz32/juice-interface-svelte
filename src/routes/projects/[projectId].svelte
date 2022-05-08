b5158851c4d0bb87024183e1555095dee522d935
Evie <evita@stenqvist.co>
2022-05-08 15:11:54 +0200

    :WIP: No antd

--- src/routes/projects/[projectId].svelte
+++ src/routes/projects/[projectId].svelte
@@ -40,40 +40,53 @@
 				}
 			]
 		});
-
 		$project = res;
+
 	});
 </script>
 
-<section
-	class="ant-layout"
-	style="display: flex; flex-direction: column; height: 100%; background: transparent;"
->
-	<div class="ant-layout-content">
-		<div style="max-width: 1080px; margin: 0px auto; padding: 20px;">
-			<div>
-				<Head />
-				<Stats />
-				<div
-					class="ant-row"
-					style="margin-left: -20px; margin-right: -20px; padding-bottom: 40px; row-gap: 0px;"
-				>
-					<Details />
-					<Activity />
-				</div>
+<section>
+	<div class="content">
+		<div>
+			<Head />
+			<Stats />
+			<div class="row">
+				<Details />
+				<Activity />
 			</div>
-			<div style="text-align: center; padding: 20px;">
-				<button type="button" class="ant-btn ant-btn-link">Back to top</button>
-			</div>
-			<a
-				class="feedback-button hide-mobile "
-				href="https://auditor.typeform.com/to/REMUTIbQ?project=juicebox&amp;address=0x283e4500fd7c9e1bf83b828f13a597b47311700c&amp;resolution=1139x984&amp;referrer=stoned-banny#"
-				target="_blank"
-				rel="noopener noreferrer"><img src="/images/stoned_banny.png" alt="Stoned banny" /></a
-			>
 		</div>
+		<div style="text-align: center; padding: 20px;">
+			<button type="button" class="ant-btn ant-btn-link">Back to top</button>
+		</div>
+		<a
+			class="feedback-button hide-mobile "
+			href="https://auditor.typeform.com/to/REMUTIbQ?project=juicebox&amp;address=0x283e4500fd7c9e1bf83b828f13a597b47311700c&amp;resolution=1139x984&amp;referrer=stoned-banny#"
+			target="_blank"
+			rel="noopener noreferrer"><img src="/images/stoned_banny.png" alt="Stoned banny" /></a
+		>
 	</div>
 </section>
 
 <style>
+	section {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		background: transparent;
+	}
+
+	.content {
+		max-width: 1080px;
+		margin: 0px auto;
+		padding: 20px;
+	}
+
+	.row {
+		display: flex;
+		flex-flow: row wrap;
+		margin-left: -20px;
+		margin-right: -20px;
+		padding-bottom: 40px;
+		row-gap: 0px;
+	}
 </style>
