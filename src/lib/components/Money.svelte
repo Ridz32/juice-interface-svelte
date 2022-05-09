a2a72cf77a2db0befc93a66557c6869bedf5a42a
Evie <evita@stenqvist.co>
2022-05-09 17:07:15 +0200

    Joint money component for handling usd/eth

--- src/lib/components/Money.svelte
+++ src/lib/components/Money.svelte
@@ -1,31 +1,25 @@
 <script lang="ts">
 	import { BigNumber } from 'ethers';
 	import { Currency } from '$constants';
-	import { formatWad, parseWad } from '$utils/formatNumber';
 	import { betweenZeroAndOne } from '$utils/bigNumbers';
 	import Ethereum from '$lib/components/Ethereum.svelte';
 	import USD from '$lib/components/USD.svelte';
+	import EthAmount from './ETHAmount.svelte';
+	import UsdAmount from './USDAmount.svelte';
 
 	export let currency: Currency = Currency.ETH;
 	export let amount = BigNumber.from('0');
-
-	// TODO combine ETHAmount with this component
-	// export let precision = 0;
-	// export let padEnd = false;
-
-	// const isBetweenZeroAndOne =
-	// 	(BigNumber.isBigNumber(amount) && betweenZeroAndOne(amount)) ||
-	// 	betweenZeroAndOne(parseWad(amount));
-
-	// const precisionAdjusted = isBetweenZeroAndOne ? 4 : precision;
-
-	// const formattedAmount = formatWad(amount, {
-	// 	precision: precisionAdjusted,
-	// 	padEnd
-	// });
+	export let formatWad = true;
+	export let precision = 0;
 </script>
 
-{#if currency === Currency.ETH}
+{#if formatWad}
+	{#if currency === Currency.ETH}
+		<EthAmount {amount} {precision} />
+	{:else}
+		<UsdAmount {amount} {precision} />
+	{/if}
+{:else if currency === Currency.ETH}
 	<Ethereum />{amount}
 {:else}
 	<USD />{amount}
