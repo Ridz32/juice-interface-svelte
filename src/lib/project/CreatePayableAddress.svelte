6f3e3aa13f48292960844b7237868d3c31bc146a
Evie <evita@stenqvist.co>
2022-05-09 23:35:51 +0200

    Create payable address modal

---
+++ src/lib/project/CreatePayableAddress.svelte
@@ -0,0 +1,52 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+	import Icon from '$lib/components/Icon.svelte';
+	import Trans from '$lib/components/Trans.svelte';
+	import { closeModal } from '$lib/components/Modal.svelte';
+
+	let isDeploying = false;
+	// TODO contract reader for deploying a new payable address
+	function deploy() {
+		isDeploying = true;
+		console.info('Not implemented yet');
+		setTimeout(() => {
+			isDeploying = false;
+			closeModal();
+		}, 2000);
+	}
+</script>
+
+<div class="content">
+	<h2>Create payable address</h2>
+	<p><Trans>Create an Ethereum address that can be used to pay your project directly.</Trans></p>
+	<p>
+		<Trans
+			>Tokens minted from payments to this address will belong to the payer. However, if someone
+			pays the project from a non-custodial entity like the Coinbase app, tokens can't be issued to
+			their personal wallets and will be lost.</Trans
+		>
+	</p>
+</div>
+<div class="buttons">
+	<Button type="secondary" size="md" on:click={closeModal}>Close</Button>
+	<Button type="primary" size="md" disabled={isDeploying} on:click={deploy}
+		>Deploy project payer contract
+		{#if isDeploying}
+			<Icon name="loading" spin />
+		{/if}
+	</Button>
+</div>
+
+<style>
+	p {
+		font-weight: 300;
+	}
+
+	h2 {
+		color: var(--text-header);
+	}
+
+	.buttons {
+		float: right;
+	}
+</style>
--- src/lib/project/ToolsDrawer.svelte
+++ src/lib/project/ToolsDrawer.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
 	import CurrencyInput from '$lib/components/CurrencyInput.svelte';
 	import Button from '$lib/components/Button.svelte';
+	import Icon from "$lib/components/Icon.svelte";
 	import Input from '$lib/components/Input.svelte';
+	import Trans from '$lib/components/Trans.svelte';
+	import { openModal } from '$lib/components/Modal.svelte';
+	import CreatePayableAddress from './CreatePayableAddress.svelte';
 
-    export let balance = 0;
+	export let balance = 0;
 	export let token = '';
 
 	let transferTokenAmount = 0;
 	let transferTokenTo: Address;
 
-    // NOTE this will be a big number due to CurrencyInput
+	// NOTE this will be a big number due to CurrencyInput
 	let addToBalanceAmount = 0;
 
 	function transfer() {
@@ -17,17 +21,32 @@
 		console.log(transferTokenTo);
 	}
 
-    function addToBalance() {
-        console.log(addToBalanceAmount);
-    }
+	function addToBalance() {
+		console.log(addToBalanceAmount);
+	}
 </script>
 
 <section>
 	<h1>Tools</h1>
 
 	<div class="item">
-		<h3>Transfer unclaimed {token}</h3>
-		<p>Your balance: {balance}</p>
+		<h3><Trans>Create payable address</Trans></h3>
+		<p><Trans>Create an Ethereum address that can be used to pay your project directly.</Trans></p>
+
+		<Button type="primary" size="md" on:click={() => openModal(CreatePayableAddress)}
+			>Deploy project payer contract</Button
+		>
+		<p class="input">
+			<Icon name="exclamationCircle" /><Trans>
+				If you have already deployed a payable address and have lost it, please contact the Juicebox
+				team through Discord.</Trans
+			>
+		</p>
+	</div>
+
+	<div class="item">
+		<h3><Trans>Transfer unclaimed {token}</Trans></h3>
+		<p><Trans>Your balance: {balance}</Trans></p>
 
 		<label for="amount">Amount</label>
 		<div class="input">
@@ -47,8 +66,8 @@
 	</div>
 
 	<div class="item">
-		<h3>Add to balance</h3>
-		<p>Add funds tho this project's balance without minting tokens.</p>
+		<h3><Trans>Add to balance</Trans></h3>
+		<p><Trans>Add funds tho this project's balance without minting tokens.</Trans></p>
 
 		<label for="payAmount">Pay amount</label>
 		<div class="input">
@@ -59,7 +78,7 @@
 				disabled
 			/>
 		</div>
-        <Button type="primary" size="md" on:click={addToBalance}>Add to balance</Button>
+		<Button type="primary" size="md" on:click={addToBalance}>Add to balance</Button>
 	</div>
 </section>
 
@@ -88,9 +107,10 @@
 	.item {
 		margin-top: 40px;
 		padding-bottom: 40px;
+		border-bottom: 1px solid var(--stroke-tertiary);
 	}
 
-	.item:first-of-type {
-		border-bottom: 1px solid var(--stroke-tertiary);
+	.item:last-of-type {
+		border: unset
 	}
 </style>
