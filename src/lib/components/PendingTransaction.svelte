d2b2d95e0ff193d0767d0370bd2797adb32f8557
Evie <evita@stenqvist.co>
2022-05-11 15:50:34 +0200

    Pending transaction component with the quint gif

---
+++ src/lib/components/PendingTransaction.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import Trans from '$lib/components/Trans.svelte';
+</script>
+
+<section>
+	<div>
+		<img
+			src="/images/quint.gif"
+			alt={'Juicebox loading animation'}
+		/>
+		<h2>
+			<Trans>Transaction pending...</Trans>
+		</h2>
+		<p>
+			<Trans>Your transaction has been submitted and is awaiting confirmation.</Trans>
+		</p>
+	</div>
+</section>
+
+<style>
+	section {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin: 2rem 0;
+	}
+
+	div {
+		max-width: 400px;
+		text-align: center;
+	}
+
+    img {
+        max-width: 100px;
+    }
+</style>
