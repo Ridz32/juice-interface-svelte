3a36d5b5d6d29babe3f896abb40ed68ff9c0f75a
Evie <evita@stenqvist.co>
2022-05-14 14:55:53 +0200

    Prompt to switch network works on initial load

--- src/lib/components/SwitchNetworkModal.svelte
+++ src/lib/components/SwitchNetworkModal.svelte
@@ -4,7 +4,7 @@
 	import { provider } from '$stores/web3';
 	import Modal, { bind } from '$lib/components/Modal.svelte';
 	import SwitchNetworkContent from './SwitchNetworkContent.svelte';
-	import { getDefaultProvider } from 'ethers';
+	import { onMount } from 'svelte';
 
 	let showModal = false;
 
@@ -15,21 +15,36 @@
 		.filter((n) => import.meta.env.VITE_INFURA_NETWORK !== n)
 		.map((_n) => {
 			const subDomain = _n === NetworkName.mainnet ? '' : _n + '.';
-			console.log(subDomain);
-			console.log(_n);
 			return {
 				label: `${subDomain}juicebox.wtf`,
 				href: `https://${subDomain}juicebox.wtf`
 			};
 		});
 
-	$: {
-		const currentProvider = $provider || getDefaultProvider(networkName);
-		showModal = currentProvider?.network?.name !== networkName;
+	function checkNetwork() {
+		showModal = $provider?.network?.name !== networkName;
 	}
+
+	onMount(() => {
+		if (!$provider?.network) {
+			// TODO this is fishy as fuck, "but it works".
+			// waits momentarily for the provider to be set
+			// by the web3 init... do this properly when time allows
+			setTimeout(() => {
+				checkNetwork();
+			}, 300);
+		}
+	});
+
+	provider.subscribe((current) => {
+		if (current?.network) {
+			checkNetwork();
+		}
+	});
 </script>
 
 <Modal
+	closeButton={false}
 	show={showModal &&
 		bind(SwitchNetworkContent, { networkName, networkOptions: supportedNetworkOptions })}
 />
